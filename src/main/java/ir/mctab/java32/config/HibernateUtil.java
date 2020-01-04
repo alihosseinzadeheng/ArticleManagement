8770b0248e58f1c53216297b462ec346e04057c1
Ali Hosseinzadeh <alihosseinzadeheng@gmail.com>
2020-01-04 20:24:45 +0330

    crud repository add

--- src/main/java/ir/mctab/java32/config/HibernateUtil.java
+++ src/main/java/ir/mctab/java32/config/HibernateUtil.java
@@ -1,16 +1,19 @@
 package ir.mctab.java32.config;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
+    private static Session session;
 
     static {
         sessionFactory = new Configuration().configure().buildSessionFactory();
+        session=sessionFactory.getCurrentSession();
     }
 
-    public static SessionFactory getSession() {
-        return sessionFactory;
+    public static Session getSession() {
+        return session;
     }
 }
--- src/main/java/ir/mctab/java32/config/repositories/CrudRepository.java
+++ src/main/java/ir/mctab/java32/config/repositories/CrudRepository.java
@@ -1,4 +1,56 @@
 package ir.mctab.java32.config.repositories;
 
-public class CrudRepository {
+
+import org.hibernate.query.Query;
+
+import java.io.Serializable;
+import java.util.List;
+
+import static ir.mctab.java32.config.HibernateUtil.getSession;
+
+public abstract class CrudRepository<Entity, ID extends Serializable> {
+
+    protected abstract Class<Entity> getEntityClass();
+
+    public Entity save(Entity entity){
+        getSession().getTransaction().begin();
+        getSession().save(entity);
+        getSession().getTransaction().commit();
+        return entity;
+    }
+    public Entity update(Entity entity){
+        getSession().beginTransaction();
+        getSession().update(entity);
+        getSession().getTransaction().commit();
+        return entity;
+    }
+    public void remove(Entity entity){
+        getSession().beginTransaction();
+        getSession().remove(entity);
+        getSession().getTransaction().commit();
+    }
+    public Entity findbyId(ID id){
+        getSession().beginTransaction();
+        Entity entity=getSession().get(getEntityClass(),id);
+        getSession().getTransaction().commit();
+        return entity;
+    }
+    public List<Entity> findAll(){
+        getSession().beginTransaction();
+        Query<Entity> query=getSession()
+                .createQuery("from"+ getEntityClass().getName(), getEntityClass());
+        List<Entity> entityList=query.list();
+        getSession().getTransaction().commit();
+        return entityList;
+    }
+    public List<Entity> findAll(int first, int max){
+        getSession().beginTransaction();
+        Query<Entity> query =
+                getSession()
+                .createQuery("from"+getEntityClass().getName(),getEntityClass());
+        query.setFirstResult(first);
+        query.setMaxResults(max);
+        List<Entity> entityList=query.list();
+        return entityList;
+    }
 }
